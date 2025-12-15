68b0effe78ca18c5ec97ad6e17e5b45213107614
Pedro Rojo <bkpedroxrojo@gmail.com>
2025-12-16 00:01:41 +0100

    add css module for login gate

--- src/assets/components/LoginGate.jsx
+++ src/assets/components/LoginGate.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import styles from './LoginGate.module.css';
 
 const LoginGate = ({handleLogin}) => {
     return (
-        <>
-            <div>Please Log In!</div>
-            <button onClick={handleLogin}>ClickToLogIn</button>
-        </>
-    )
+        <div className={styles.container}>
+            <h1 className={styles.title}>SPOTIFYND</h1>
+            <button className={styles.button} onClick={handleLogin}>
+                Log in to Auth
+            </button>
+            <div className={styles.footer}>
+                Made with React by Pedro Rojo.
+            </div>
+        </div>
+    );
 }
 
 export default LoginGate;
